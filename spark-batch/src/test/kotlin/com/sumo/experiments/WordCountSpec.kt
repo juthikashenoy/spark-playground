9ee842927a186a42d9395d968dbdad37165142bc
Sumanth Chinthagunta <xmlking@gmail.com>
2016-06-10 10:13:39 -0700

    initial commit

---
+++ spark-batch/src/test/kotlin/com/sumo/experiments/WordCountSpec.kt
@@ -0,0 +1,11 @@
+package com.sumo.experiments
+
+import com.nhaarman.mockito_kotlin.mock
+import org.jetbrains.spek.api.Spek
+import kotlin.test.assertEquals
+
+class WordCountSpec : Spek( {
+    given("a WordCountSpec") {
+
+    }
+})
